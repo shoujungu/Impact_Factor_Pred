9981775cd296456f2f984afabfae89e6a2040575
Shoujun <shoujun.gu@gmail.com>
2018-04-20 00:11:47 -0400

    sample

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def my_form_post():
 
 if __name__=='__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
